833ed8eace2366f557e7f6d5ed44bb0c836e3977
Teguh Sitorus <47329758+teguhsitorus@users.noreply.github.com>
2024-01-21 23:13:21 +0800

    Update MultiPaperServer.java

--- MultiPaper-Master/src/main/java/puregero/multipaper/server/MultiPaperServer.java
+++ MultiPaper-Master/src/main/java/puregero/multipaper/server/MultiPaperServer.java
@@ -18,7 +18,7 @@ public class MultiPaperServer extends MessageBootstrap<MasterBoundMessage, Serve
     public static void main(String[] args) throws IOException {
         DAEMON = false;
 
-        String address = null;
+        String address = 127.0.0.1;
         int port = DEFAULT_PORT;
 
         if (args.length > 0) {
